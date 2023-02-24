6de2bd98eb8c9448620c2b3a1b010119c5cb66ce
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-02-24 17:17:11 +0530

    added hotel page

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -1,9 +1,11 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "../Pages/Home";
+import Hotel from "../Pages/Hotel";
 
 function AllRoutes(){
     return <Routes>
         <Route path="/" element={<Home />} ></Route>
+        <Route path="/hotel" element={<Hotel />} ></Route>
     </Routes>
 }
 
